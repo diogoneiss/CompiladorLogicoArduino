a924f7a28272efc2eaf5fd4d9902f490d3d99caf
diogoneiss <diogo.oliveiran@gmail.com>
2020-04-08 21:21:31 -0300

    Objeto linha criado

--- Parser.java
+++ Parser.java
@@ -19,7 +19,7 @@ public class Parser {
 
     public int a;
     public int b;
-    public Operacoes listaOps;
+
 
     public static void main(String[] args){
 
@@ -29,20 +29,21 @@ public class Parser {
         entrada = entrada.replace("fim.", "");
         //corte do arquivo em múltiplas linhas
         String[] entradaCortada = entrada.split(";");
+        LinhaInstrucao[] listaInstrucoes = new LinhaInstrucao[entradaCortada.length];
 
-       /* for (String l : entradaCortada) {
-            System.out.println(l);
-        }*/
+        //construir os objetos
+       for (int i = 0; i < entradaCortada.length; i++) {
+            listaInstrucoes[i] = new LinhaInstrucao(entradaCortada[i]);
+           System.out.print(listaInstrucoes[i]);
+       }
 
-        System.out.println("Fim do programa, é isto.");
-
-
+       System.out.println("Fim do programa, é isto.");
 
     }
 
     /**
      * @author Diogo Neiss
-     * @param fileName
+     * @param fileName nome do arquivo na pasta corrente
      * @return linha completa do arquivo
      */
     public static String readFileAsString(String fileName)
@@ -66,6 +67,36 @@ class LinhaInstrucao {
 
     }
     LinhaInstrucao(String fraseFiltrar){
+        limparString(fraseFiltrar);
+    }
 
+    @Override
+    public String toString() {
+        return this.linhaComInstrucao;
+    }
+
+    /**
+     * Converter a string splitada em um objeto devido e organizado.
+     * @param frasefiltrar frase a ser filtrada
+     */
+    private void limparString(String frasefiltrar){
+        if(frasefiltrar.equals(" ") && frasefiltrar.equals("")){
+            frasefiltrar += ';';
+
+            if(frasefiltrar.contains("=A"))
+                frasefiltrar = frasefiltrar.replace("=A", "=10");
+            if(frasefiltrar.contains("=B"))
+                frasefiltrar = frasefiltrar.replace("=B", "=11");
+            if(frasefiltrar.contains("=C"))
+                frasefiltrar = frasefiltrar.replace("=C", "=12");
+            if(frasefiltrar.contains("=D"))
+                frasefiltrar = frasefiltrar.replace("=D", "=13");
+            if(frasefiltrar.contains("=E"))
+                frasefiltrar = frasefiltrar.replace("=E", "=14");
+            if(frasefiltrar.contains("=F"))
+                frasefiltrar = frasefiltrar.replace("=F", "=15");
+
+            this.linhaComInstrucao = frasefiltrar;
+        }
     }
 }
