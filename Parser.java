05c35cc9406b1af46f4cd66e437a827098ad3e51
diogoneiss <diogo.oliveiran@gmail.com>
2020-04-08 21:32:53 -0300

    arrumei tab space

--- Parser.java
+++ Parser.java
@@ -17,86 +17,87 @@ fim.
 
 public class Parser {
 
-    public int a;
-    public int b;
+	public int a;
+	public int b;
 
 
-    public static void main(String[] args){
+	public static void main(String[] args) {
 
-        //leitura do arquivo como uma única linha, que será splitada em múltiplas instruçoes
-        String entrada = (readFileAsString("testeula.ula"));
-        entrada = entrada.replace("inicio:", "");
-        entrada = entrada.replace("fim.", "");
-        //corte do arquivo em múltiplas linhas
-        String[] entradaCortada = entrada.split(";");
-        LinhaInstrucao[] listaInstrucoes = new LinhaInstrucao[entradaCortada.length];
+		//leitura do arquivo como uma única linha, que será splitada em múltiplas instruçoes
+		String entrada = (readFileAsString("testeula.ula"));
+		entrada = entrada.replace("inicio:", "");
+		entrada = entrada.replace("fim.", "");
+		//corte do arquivo em múltiplas linhas
+		String[] entradaCortada = entrada.split(";");
+		LinhaInstrucao[] listaInstrucoes = new LinhaInstrucao[entradaCortada.length];
 
-        //construir os objetos
-       for (int i = 0; i < entradaCortada.length; i++) {
-            listaInstrucoes[i] = new LinhaInstrucao(entradaCortada[i]);
-           System.out.print(listaInstrucoes[i]);
-       }
+		//construir os objetos
+		for (int i = 0; i < entradaCortada.length; i++) {
+			listaInstrucoes[i] = new LinhaInstrucao(entradaCortada[i]);
+			System.out.print(listaInstrucoes[i]);
+		}
 
-       System.out.println("Fim do programa, é isto.");
+		System.out.println("Fim do programa, é isto.");
 
-    }
+	}
 
-    /**
-     * @author Diogo Neiss
-     * @param fileName nome do arquivo na pasta corrente
-     * @return linha completa do arquivo
-     */
-    public static String readFileAsString(String fileName)
-    {
-        String data = "";
-        try{
-            data = new String(Files.readAllBytes(Paths.get(fileName)) );
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+	/**
+	 * @param fileName nome do arquivo na pasta corrente
+	 * @return linha completa do arquivo
+	 * @author Diogo Neiss
+	 */
+	public static String readFileAsString(String fileName) {
+		String data = "";
+		try {
+			data = new String(Files.readAllBytes(Paths.get(fileName)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return data;
-    }
+		return data;
+	}
 
 }
 
 class LinhaInstrucao {
-    public String linhaComInstrucao;
+	public String linhaComInstrucao;
 
-    LinhaInstrucao(){
+	LinhaInstrucao() {
 
-    }
-    LinhaInstrucao(String fraseFiltrar){
-        limparString(fraseFiltrar);
-    }
+	}
 
-    @Override
-    public String toString() {
-        return this.linhaComInstrucao;
-    }
+	LinhaInstrucao(String fraseFiltrar) {
+		limparString(fraseFiltrar);
+	}
 
-    /**
-     * Converter a string splitada em um objeto devido e organizado.
-     * @param frasefiltrar frase a ser filtrada
-     */
-    private void limparString(String frasefiltrar){
-        if(frasefiltrar.equals(" ") && frasefiltrar.equals("")){
-            frasefiltrar += ';';
+	@Override
+	public String toString() {
+		return this.linhaComInstrucao;
+	}
 
-            if(frasefiltrar.contains("=A"))
-                frasefiltrar = frasefiltrar.replace("=A", "=10");
-            if(frasefiltrar.contains("=B"))
-                frasefiltrar = frasefiltrar.replace("=B", "=11");
-            if(frasefiltrar.contains("=C"))
-                frasefiltrar = frasefiltrar.replace("=C", "=12");
-            if(frasefiltrar.contains("=D"))
-                frasefiltrar = frasefiltrar.replace("=D", "=13");
-            if(frasefiltrar.contains("=E"))
-                frasefiltrar = frasefiltrar.replace("=E", "=14");
-            if(frasefiltrar.contains("=F"))
-                frasefiltrar = frasefiltrar.replace("=F", "=15");
+	/**
+	 * Converter a string splitada em um objeto devido e organizado.
+	 *
+	 * @param frasefiltrar frase a ser filtrada
+	 */
+	private void limparString(String frasefiltrar) {
+		if (frasefiltrar.equals(" ") && frasefiltrar.equals("")) {
+			frasefiltrar += ';';
 
-            this.linhaComInstrucao = frasefiltrar;
-        }
-    }
+			if (frasefiltrar.contains("=A"))
+				frasefiltrar = frasefiltrar.replace("=A", "=10");
+			if (frasefiltrar.contains("=B"))
+				frasefiltrar = frasefiltrar.replace("=B", "=11");
+			if (frasefiltrar.contains("=C"))
+				frasefiltrar = frasefiltrar.replace("=C", "=12");
+			if (frasefiltrar.contains("=D"))
+				frasefiltrar = frasefiltrar.replace("=D", "=13");
+			if (frasefiltrar.contains("=E"))
+				frasefiltrar = frasefiltrar.replace("=E", "=14");
+			if (frasefiltrar.contains("=F"))
+				frasefiltrar = frasefiltrar.replace("=F", "=15");
+
+			this.linhaComInstrucao = frasefiltrar;
+		}
+	}
 }
